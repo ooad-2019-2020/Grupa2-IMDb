36e1b9c5d42037de68ede38923e499f4511269ae
tylerwetrust <mucanbarli1@etf.unsa.ba>
2020-05-28 00:09:21 +0200

    film detial view

--- Implementacija/MovieHub/MovieHub/Startup.cs
+++ Implementacija/MovieHub/MovieHub/Startup.cs
@@ -45,6 +45,8 @@ namespace MovieHub
             */
             services.AddControllersWithViews();
             services.AddRazorPages();
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
